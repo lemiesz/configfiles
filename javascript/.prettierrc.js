9ee2a1a05523289615bbfb4d0e155a4edeadb11d
Robert Lemiesz <lemiesz@amazon.com>
2019-12-11 12:00:08 -0800

    Adding my default prettier file

---
+++ javascript/.prettierrc.js
@@ -0,0 +1,8 @@
+// Baseline prettier file I use in a lot of projects
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    arrowParens: 'always',
+    trailingComma: 'all',
+};
